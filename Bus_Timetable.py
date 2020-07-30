7c561338dab1b3714bc3bd746ca854e63edca7f5
jhLim97 <wnsgur2749@naver.com>
2020-07-31 03:44:50 +0900

    PUSH

---
+++ Bus_Timetable.py
@@ -0,0 +1,29 @@
+from selenium import webdriver as wd
+import sys
+from PyQt5 import uic
+from PyQt5.QtWidgets import *
+
+UI_FORM = uic.loadUiType("Bus_Timetable.ui")[0]
+
+class MyWindow(QMainWindow, UI_FORM):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+        self.pushButton.clicked.connect(self.get_number_and_timetable)
+
+    def get_number_and_timetable(self):
+        Bus_number = self.lineEdit.text()
+        browser = wd.Chrome('[파일경로]/chromedriver.exe') #권한 거부 오류 시 실행파일명까지 쓰면 실행됨.
+        url = "http://bus.jeju.go.kr/schedule/view/" + Bus_number
+        browser.get(url)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWindow = MyWindow()
+    myWindow.show()
+    app.exec_()
+
+
+
+
