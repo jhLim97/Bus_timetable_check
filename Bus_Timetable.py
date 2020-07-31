7b438386e539c12e34346a7706e5644a23e98c93
Gerrard3016 <67899069+jhLim97@users.noreply.github.com>
2020-08-01 01:09:55 +0900

    Update Bus_Timetable.py

--- Bus_Timetable.py
+++ Bus_Timetable.py
@@ -9,14 +9,26 @@ class MyWindow(QMainWindow, UI_FORM):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        
+        self.timer = QTimer(self)
+        self.timer.start(1000)
+        self.timer.timeout.connect(self.timeout)
 
         self.pushButton.clicked.connect(self.get_number_and_timetable)
 
     def get_number_and_timetable(self):
         Bus_number = self.lineEdit.text()
-        browser = wd.Chrome('[파일경로]/chromedriver.exe') #파일경로 부분은 본인이 chromegriver설치한 경로. 권한 거부 오류 시 실행파일명까지 쓰면 실행됨.
+        browser = wd.Chrome('[파일경로]/chromedriver.exe') #파일경로 부분은 본인이 chromedriver설치한 경로. 권한 거부 오류 시 실행파일명까지 쓰면 실행됨.
         url = "http://bus.jeju.go.kr/schedule/view/" + Bus_number
         browser.get(url)
+        
+    def timeout(self):
+        current_time = QTime.currentTime()
+
+        text_time = current_time.toString("hh:mm:ss")
+        time_msg = "현재시간: " + text_time
+
+        self.statusbar.showMessage(time_msg)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
